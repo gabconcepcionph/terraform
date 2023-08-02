944261bb6e4cc45f0337946608de4ac03733b3c4
gabconcepcionph <41775336+gabconcepcionph@users.noreply.github.com>
2023-08-02 10:27:57 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,15 +6,15 @@ locals {
 
 variable "project" {
     type= string
-    description = "terraform-394700"
+    default = "terraform-394700"
 }
 
 variable "region" {
     type= string
-    description = "asia-east1"
+    default = "asia-east1"
 }
 
 variable  "data-project" {
     type = string
-    description = "Name of data pipeline project to use as resource prefix"
+    default = "Name of data pipeline project to use as resource prefix"
 }
