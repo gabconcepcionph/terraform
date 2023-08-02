cb5946863bd347d502f7c243148a2693bd56ba8d
gabconcepcionph <41775336+gabconcepcionph@users.noreply.github.com>
2023-08-02 16:03:49 +0800

    Update network.tf

--- network.tf
+++ network.tf
@@ -10,3 +10,16 @@ resource "google_compute_subnetwork" "default" {
   region        = var.region
   network       = google_compute_network.vpc_network.id
 }
+
+resource "google_compute_firewall" "ssh" {
+  name = "allow-ssh"
+  allow {
+    ports    = ["22"]
+    protocol = "tcp"
+  }
+  direction     = "INGRESS"
+  network       = google_compute_network.vpc_network.id
+  priority      = 1000
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["ssh"]
+}
